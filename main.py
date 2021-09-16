c644d404069e3b9abd29f677cabc14e199db8a8e
Billy Green <44146108+BillyGreenGreen@users.noreply.github.com>
2021-09-16 20:01:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from SwSpotify import spotify
 import time
 
-while True:
-    time.sleep(3)
-    try:
-        with open("song.txt", "w+") as f:
-            line = f.readline()
-            song = spotify.artist() + " - " + spotify.song()
-            if len(song) > 42:
-                sub = len(song) - 42
-                song = song[:-sub] + "..."
-            print(line)
-            print(song)
-            if line != song:
-                f.write(song)
-                f.close()
+if __name__ == "__main__":
+    while True:
+        time.sleep(3)
+        try:
+            with open("song.txt", "w+") as f:
+                line = f.readline()
+                song = spotify.artist() + " - " + spotify.song()
+                if len(song) > 42:
+                    sub = len(song) - 42
+                    song = song[:-sub] + "..."
+                print(line)
+                print(song)
+                if line != song:
+                    f.write(song)
+                    f.close()
 
-    except:
-        with open("song.txt", "w") as f:
-            f.write("No song currently playing...")
-            f.close()
+        except:
+            with open("song.txt", "w") as f:
+                f.write("No song currently playing...")
+                f.close()
